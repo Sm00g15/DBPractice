65fd282dc0f086a47951f8853d144f96ae76fa8b
Sean M <sean@arrowburn.co.nz>
2018-05-14 19:56:43 +1200

    validation/testing for validation added

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
 	let joe;
 
 	beforeEach((done) => {
-		joe = new User({name: "Joe"});
+		joe = new User({name: "Joe", postCount: 0});
 		joe.save()
 			.then(() => done());
 	})
@@ -36,5 +36,13 @@ function assertName(operation, done) {
 	})
 	it('A model class can find a record with an id and update', (done) => {
 		assertName(User.findByIdAndUpdate(joe._id, {name: 'Alex'}), done);
+	})
+	it('A user can have their post count incremented by 1', (done) =>{
+		User.update({name: "Joe"}, { $inc: {postCount: 1}})
+			.then(() => User.findOne({name: "Joe"}))
+			.then((user) => {
+				assert(user.postCount === 1);
+				done();
+			})
 	})
 })
